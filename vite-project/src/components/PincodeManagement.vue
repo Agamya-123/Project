56d0b0fc24ef2cb04a9989c5228f2c0b1e731afd
Agamya <agamyarathour@fynd.team>
2025-06-25 17:40:49 +0530

    Fix: the model overlay box shadow colour

--- vite-project/src/components/PincodeManagement.vue
+++ vite-project/src/components/PincodeManagement.vue
@@ -592,7 +592,7 @@ input:focus, select:focus {
 .add-pin-content {
   background: rgba(255, 255, 255, 0.15);
   border-radius: 20px;
-  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.37);
   backdrop-filter: blur(14px);
   -webkit-backdrop-filter: blur(14px);
   border: 1px solid rgba(255, 255, 255, 0.18);
